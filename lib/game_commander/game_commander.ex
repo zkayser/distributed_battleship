15cc4a99972a201da25360374aabbc0a6bf066cf
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-10-17 20:20:07 -0400

    add prototype fsm for full sequnece. not sure how to manage ticks vs state changes

--- lib/game_commander/game_commander.ex
+++ lib/game_commander/game_commander.ex
@@ -1,11 +1,29 @@
 defmodule GameCommander do
 
+  # how would a full fsm work?
+  def noop(_) do
+  end
+
+  def fsm() do
+    %{
+      #Old State,         Event      Action,                   New State
+      {:none,             :next} => {&WaitingForPlayers.run/1, :wait_for_players},
+      {:wait_for_players, :next} => {&StartGame.run/1,         :start_game},
+      {:start_game,       :next} => {&GameCommander.noop/1,    :adding_ships},
+      {:adding_ships,     :next} => {&GameCommander.noop/1,    :taking_turns},
+      {:taking_turns,     :next} => {&GameCommander.noop/1,    :feedback},
+      {:feedback,         :next} => {&GameCommander.noop/1,    :scoreboard}
+    }
+  end
+
+  # Simple state sequence
   @fsm %{
+    :none             => :wait_for_players,
     :wait_for_players => :start_game,
-    :start_game => :adding_ships,
-    :adding_ships => :taking_turns,
-    :taking_turns => :feedback,
-    :feedback => :scoreboard
+    :start_game       => :adding_ships,
+    :adding_ships     => :taking_turns,
+    :taking_turns     => :feedback,
+    :feedback         => :scoreboard
   }
 
   def new() do
