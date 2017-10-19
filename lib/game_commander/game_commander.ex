161887ca914b31c0ddcfcf282ba29caa7f64b0a7
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-10-18 20:09:32 -0400

    add repeated ticks with a finish condition.

--- lib/game_commander/game_commander.ex
+++ lib/game_commander/game_commander.ex
@@ -1,9 +1,18 @@
 defmodule GameCommander do
-  def start(state, actions) do
-    context = %{}
+  def start(state, context, actions) do
+    play(state, context, actions)
+  end
 
-    actions[state].(context)
+  defp play(:finish, context, _), do: context
+  defp play(state, context, actions) do
+    context = actions[state].(context)
 
-    Map.update(context, :tick_count, 1, &(&1 + 1))
+    new_state = context.new_state
+    context = Map.delete(context, :new_state)
+    context = Map.update(context, :tick_count, 1, &(&1 + 1))
+
+    IO.puts(">>>> #{state} => #{new_state} : #{inspect context}")
+
+    play(new_state, context, actions)
   end
 end
--- test/game_commander/game_commander_test.exs
+++ test/game_commander/game_commander_test.exs
@@ -2,14 +2,25 @@ defmodule GameCommanderTest do
   use ExUnit.Case
 
   def fake_tick(context) do
-    context
+    Map.merge(context, %{new_state: hd(context.states), states: tl(context.states)})
   end
 
   describe "tick" do
     test "trigger an action on a tick" do
-      context = GameCommander.start(:none, %{none: &GameCommanderTest.fake_tick/1})
+      context = GameCommander.start(:none, %{states: [:finish]}, %{none: &GameCommanderTest.fake_tick/1})
 
       assert context.tick_count == 1
+    end
+
+    test "run two ticks" do
+      states = %{
+        none: &GameCommanderTest.fake_tick/1,
+        finish: &GameCommanderTest.fake_tick/1
+      }
+
+      context = GameCommander.start(:none, %{states: [:none, :finish]}, states)
+
+      assert context.tick_count == 2
     end
   end
 
