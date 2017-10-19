f7f3c7a83daf5edc0acf5f53244235f3bed14077
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-10-18 20:51:11 -0400

    add full state action table

--- lib/game_commander/game_commander.ex
+++ lib/game_commander/game_commander.ex
@@ -1,11 +1,41 @@
 defmodule GameCommander do
+  @actions [
+    none:             &GameCommander.noop/1,
+    wait_for_players: &GameCommander.noop/1,
+    start_game:       &GameCommander.noop/1,
+    adding_ships:     &GameCommander.noop/1,
+    taking_turns:     &GameCommander.noop/1,
+    feedback:         &GameCommander.noop/1,
+    scoreboard:       &GameCommander.noop/1,
+    finish:           &GameCommander.noop/1
+  ]
+
+  def noop(context) do
+    Map.merge(context, %{new_state: context.state})
+  end
+
+  def states() do
+    Keyword.keys(@actions)
+  end
+
+  def actions() do
+    @actions
+  end
+
+  def start(state, context) do
+    play(state, context, @actions)
+  end
+
   def start(state, context, actions) do
     play(state, context, actions)
   end
 
   defp play(:finish, context, _), do: context
   defp play(state, context, actions) do
-    context = actions[state].(context)
+    context = Map.merge(context, %{state: state})
+
+    action = Keyword.get(actions, state)
+    context = action.(context)
 
     new_state = context.new_state
     context = Map.delete(context, :new_state)
--- test/game_commander/game_commander_test.exs
+++ test/game_commander/game_commander_test.exs
@@ -7,16 +7,16 @@ defmodule GameCommanderTest do
 
   describe "tick" do
     test "trigger an action on a tick" do
-      context = GameCommander.start(:none, %{states: [:finish]}, %{none: &GameCommanderTest.fake_tick/1})
+      context = GameCommander.start(:none, %{states: [:finish]}, [none: &GameCommanderTest.fake_tick/1])
 
       assert context.tick_count == 1
     end
 
     test "run two ticks" do
-      states = %{
+      states = [
         none: &GameCommanderTest.fake_tick/1,
         finish: &GameCommanderTest.fake_tick/1
-      }
+      ]
 
       context = GameCommander.start(:none, %{states: [:none, :finish]}, states)
 
@@ -24,6 +24,16 @@ defmodule GameCommanderTest do
     end
   end
 
+  describe "all the states" do
+    test "work through each state" do
+      fake_actions = Enum.map(GameCommander.actions, fn {name, _} ->
+        {name, &GameCommanderTest.fake_tick/1}
+      end)
 
+      context = GameCommander.start(:none, %{states: GameCommander.states}, fake_actions)
+
+      assert context.tick_count == 8
+    end
+  end
 end
 
