cdfb51536ccc26c155f10b7028809b49e5eff2bc
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-10-19 20:30:44 -0400

    refactor to improve test variable names

--- test/game_commander/game_commander_test.exs
+++ test/game_commander/game_commander_test.exs
@@ -2,23 +2,23 @@ defmodule GameCommanderTest do
   use ExUnit.Case
 
   def fake_tick(context) do
-    Map.merge(context, %{new_state: hd(context.states), states: tl(context.states)})
+    Map.merge(context, %{new_state: hd(context.test_states), test_states: tl(context.test_states)})
   end
 
   describe "tick" do
     test "trigger an action on a tick" do
-      context = GameCommander.start(:none, %{states: [:finish]}, [none: &GameCommanderTest.fake_tick/1])
+      context = GameCommander.start(:none, %{test_states: [:finish]}, [none: &GameCommanderTest.fake_tick/1])
 
       assert context.tick_count == 1
     end
 
     test "run two ticks" do
-      states = [
+      actions = [
         none: &GameCommanderTest.fake_tick/1,
         finish: &GameCommanderTest.fake_tick/1
       ]
 
-      context = GameCommander.start(:none, %{states: [:none, :finish]}, states)
+      context = GameCommander.start(:none, %{test_states: [:none, :finish]}, actions)
 
       assert context.tick_count == 2
     end
@@ -26,11 +26,11 @@ defmodule GameCommanderTest do
 
   describe "all the states" do
     test "work through each state" do
-      fake_actions = Enum.map(GameCommander.actions, fn {name, _} ->
+      actions = Enum.map(GameCommander.actions, fn {name, _} ->
         {name, &GameCommanderTest.fake_tick/1}
       end)
 
-      context = GameCommander.start(:none, %{states: GameCommander.states}, fake_actions)
+      context = GameCommander.start(:none, %{test_states: GameCommander.states}, actions)
 
       assert context.tick_count == 8
     end
