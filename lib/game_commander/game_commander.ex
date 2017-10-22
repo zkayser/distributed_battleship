b019928abcd485970397cefc431447e582446011
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-10-22 17:23:40 -0400

    correct name of wait for players action

--- lib/game_commander/game_commander.ex
+++ lib/game_commander/game_commander.ex
@@ -4,7 +4,7 @@ defmodule GameCommander do
 
   @actions [
     none:             &GameCommander.noop/1,
-    wait_for_players: &WaitingForPlayers.run/1,
+    wait_for_players: &WaitingForPlayers.tick/1,
     start_game:       &GameCommander.noop/1,
     adding_ships:     &GameCommander.noop/1,
     taking_turns:     &GameCommander.noop/1,
