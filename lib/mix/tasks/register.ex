2364b38ca13b33d1cadd9bf53b5d0ef77d8278b4
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-10-26 14:56:55 -0400

    connect the mix tasks up with the players process

--- lib/mix/tasks/register.ex
+++ lib/mix/tasks/register.ex
@@ -5,5 +5,17 @@ defmodule Mix.Tasks.Battleship.Register do
 
   def run(name) do
     IO.puts "Player #{name} registering."
+
+    {:ok, hostname} = :inet.gethostname
+    connected = Node.connect :"commander@#{hostname}"
+    IO.puts(">>>> Connected #{connected}")
+
+    :timer.sleep(1000)
+
+    pid = :global.whereis_name(:players)
+    IO.puts(">>>> players pid #{inspect pid}")
+
+    players = Players.register(pid, name)
+    IO.puts(">>>> #{inspect players}")
   end
 end
--- lib/mix/tasks/registered_players.ex
+++ lib/mix/tasks/registered_players.ex
@@ -1,9 +1,24 @@
 defmodule Mix.Tasks.Battleship.RegisteredPlayers do
   use Mix.Task
+  
+  require Players
 
   @shortdoc "A list of registered players"
 
   def run(_) do
     IO.puts "Registered Players"
+
+    {:ok, hostname} = :inet.gethostname
+    connected = Node.connect :"commander@#{hostname}"
+    IO.puts(">>>> Connected #{connected}")
+
+    :timer.sleep(1000)
+
+    pid = :global.whereis_name(:players)
+    IO.puts(">>>> players pid #{inspect pid}")
+
+    players = Players.registered_players(pid)
+    IO.puts(">>>> registered players #{inspect players}")
+
   end
 end
