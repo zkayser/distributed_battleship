70655e01db9849694c99fff54d7727c7a59a9289
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-11-13 16:39:48 -0500

    add orientation calculation

--- lib/battleship/collision.ex
+++ lib/battleship/collision.ex
@@ -1,5 +1,13 @@
 defmodule Collision do
-  def orientation(point1, point2, point3) do
-  
+  # http://www.geeksforgeeks.org/orientation-3-ordered-points/
+  # 0,0 is top left.
+  def orientation({point1_y, point1_x}, {point2_y, point2_x}, {point3_y, point3_x }) do
+
+    value = (point2_y - point1_y) * (point3_x - point2_x) - (point2_x - point1_x) * (point3_y - point2_y)
+    cond do
+      value == 0 -> :colinear
+      value < 0  -> :clockwise
+      value > 0  -> :counterclockwise
+    end
   end
 end
--- test/battleship/collision_test.exs
+++ test/battleship/collision_test.exs
@@ -2,19 +2,18 @@ defmodule CollisionTest do
   use ExUnit.Case
 
   describe "orientation" do
-    @tag :skip
     test "clockwise" do
       assert :clockwise == Collision.orientation({1,1},{1,2},{2,1})
+      assert :clockwise == Collision.orientation({1,1},{3,2},{2,1})
+      assert :clockwise == Collision.orientation({10,20},{3,12},{3,30})
     end
 
-    @tag :skip
     test "counter clockwise" do
       assert :counterclockwise == Collision.orientation({2,1},{2,2},{1,1})
     end
   
-    @tag :skip
-    test "collinear" do
-      assert :collinear == Collision.orientation({1,1},{1,2},{1,3})
+    test "colinear" do
+      assert :colinear == Collision.orientation({1,1},{1,2},{1,3})
     end
   end
 end
