f8ebf2746c2ded9d6fcbb71ec876605b79d7b173
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-11-13 18:20:25 -0500

    complete ship collision algorithm

--- lib/battleship/collision.ex
+++ lib/battleship/collision.ex
@@ -10,4 +10,40 @@ defmodule Collision do
       value > 0  -> :counterclockwise
     end
   end
+
+  # http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+  def intersect(
+        line1_point1 = {_, _}, 
+        line1_point2 = {_, _},
+        line2_point1 = {_, _}, 
+        line2_point2 = {_, _}) do
+
+    orientation_1_1 = orientation(line1_point1, line1_point2, line2_point1)
+    orientation_1_2 = orientation(line1_point1, line1_point2, line2_point2)
+    orientation_2_1 = orientation(line2_point1, line2_point2, line1_point1)
+    orientation_2_2 = orientation(line2_point1, line2_point2, line1_point2)
+
+    cond do
+      # General Case
+      orientation_1_1 != orientation_1_2 && orientation_2_1 != orientation_2_2 -> true
+
+      # Special Cases
+      orientation_1_1 == :colinear && on_line(line1_point1, line1_point2, line2_point1) -> true
+      orientation_1_2 == :colinear && on_line(line1_point1, line1_point2, line2_point2) -> true
+      orientation_2_1 == :colinear && on_line(line2_point1, line2_point2, line1_point1) -> true
+      orientation_2_2 == :colinear && on_line(line2_point1, line2_point2, line1_point2) -> true
+      true                                                                              -> false
+    end
+  end
+
+  def on_line({line_point1_x, line_point1_y}, 
+                 {line_point2_x, line_point2_y},
+                 {point_x, point_y}) do
+    cond do
+      point_x in min(line_point1_x, line_point2_x)..max(line_point1_x, line_point2_x)  &&
+        point_y in min(line_point1_y, line_point2_y)..max(line_point1_y, line_point2_y) -> true
+      true                                                                              -> false
+    end
+  end
+
 end
--- test/battleship/collision_test.exs
+++ test/battleship/collision_test.exs
@@ -16,5 +16,67 @@ defmodule CollisionTest do
       assert :colinear == Collision.orientation({1,1},{1,2},{1,3})
     end
   end
+
+  describe "lines intersecting" do
+    test "simple crosses" do
+      assert Collision.intersect({1,1}, {10,10}, {10,1}, {1,10})
+    end
+
+    test "parallel horizontal" do
+      refute Collision.intersect({1,1}, {1,10}, {10,1}, {10,10})
+    end
+
+    test "parallel vertical" do
+      refute Collision.intersect({1,1}, {10,1}, {1,10}, {10,10})
+    end
+  end
+
+  describe "on a horizontal line" do
+    test "in middle" do
+      assert Collision.on_line({1,1}, {1,10}, {1,5})
+    end
+
+    test "on one end" do
+      assert Collision.on_line({1,1}, {1,10}, {1,10})
+      assert Collision.on_line({1,1}, {1,10}, {1,1})
+    end
+
+    test "reversed points" do
+      assert Collision.on_line({1,10}, {1,1}, {1,5})
+    end
+
+    test "above the line" do
+      refute Collision.on_line({5,1}, {5,10}, {2,5})
+    end
+
+    test "below the line" do
+      refute Collision.on_line({1,1}, {1,10}, {2,5})
+    end
+    
+  end
+
+  describe "on a vertical line" do
+    test "in middle" do
+      assert Collision.on_line({1,1}, {10,1}, {5,1})
+    end
+
+    test "on one end" do
+      assert Collision.on_line({1,1}, {10,1}, {10,1})
+      assert Collision.on_line({1,1}, {10,1}, {1,1})
+    end
+
+    test "reversed points" do
+      assert Collision.on_line({10,1}, {1,1}, {5,1})
+    end
+
+    test "left the line" do
+      refute Collision.on_line({1,5}, {10,5}, {5,2})
+    end
+
+    test "right the line" do
+      refute Collision.on_line({1,1}, {10,1}, {5,2})
+    end
+    
+  end
 end
 
