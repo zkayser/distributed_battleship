e83f761ec3a08c8ab2fff1998f93dc3390df4216
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-11-28 19:05:21 -0500

    refactor commander modules into their own files

---
+++ lib/battleship/commander/organize.ex
@@ -0,0 +1,58 @@
+defmodule Organize do
+  require Logger
+
+  def play(phase, context, phases) do
+    context
+    |> track_phase(phase)
+    |> for_phase(phase)
+    |> run_phase(phases)
+    |> update_tick_count()
+    |> wait_for_tick()
+    |> logger()
+  end
+
+  defp for_phase(context, phase) do
+    context
+      |> Map.merge(%{phase: phase})
+  end
+
+  defp run_phase(context, phases) do
+    Phase.run(context, phases)
+  end
+
+  defp update_tick_count(context) do
+    context
+    |> Map.update(:tick_count, 1, &(&1 + 1))
+  end
+
+  defp wait_for_tick(context) do
+    pause(context[:tick_rate_ms])
+    context
+  end
+
+  defp logger(context = %{old_phase: old_phase, phase: new_phase}) do
+    Logger.debug("#{__MODULE__}: PHASE #{old_phase} => #{new_phase} : #{inspect Map.delete(context, :track_phase)}")
+    context
+  end
+  defp logger(context = %{phase: new_phase}) do
+    Logger.debug("#{__MODULE__}: PHASE _____ => #{new_phase} : #{inspect Map.delete(context, :track_phase)}")
+    context
+  end
+  defp logger(context) do
+    Logger.debug("#{__MODULE__}: PHASE _____ => _____ : #{inspect Map.delete(context, :track_phase)}")
+    context
+  end
+
+  defp pause(nil), do: :ok
+  defp pause(tick_rate_ms) do
+    :timer.sleep(tick_rate_ms)
+  end
+
+  def track_phase(context = %{track_phase: track_phase}, phase) do
+    %{context | track_phase: track_phase ++ [phase]}
+  end
+  def track_phase(context, phase) do
+    Map.merge(context, %{track_phase: [phase]})
+  end
+end
+
---
+++ lib/battleship/commander/phase.ex
@@ -0,0 +1,36 @@
+defmodule Phase do
+  def run(context, phases) do
+    for_phase_context(context)
+    |> setup_phase_parameters(context)
+    |> run_phase_action(context.phase, phases)
+    |> update_context(context) 
+  end
+
+  def for_phase_context(context) do
+    phase = context.phase
+    case context do
+      %{^phase => phase_context} -> phase_context
+      _                          -> %{}
+    end
+  end
+
+  def setup_phase_parameters(phase_context, %{service: service}) do
+    Map.merge(phase_context, %{service: service})
+  end
+  def setup_phase_parameters(phase_context, _) do
+    phase_context
+  end
+
+  def run_phase_action(phase_context, phase, phases) do
+    phase_action = Keyword.get(phases, phase)
+    phase_action.(phase_context)
+  end
+
+  def update_context(phase_context = %{new_phase: new_phase}, context) do
+    Map.merge(context, %{context.phase => phase_context, old_phase: context.phase, phase: new_phase})
+  end
+  def update_context(phase_context, context) do
+    Map.merge(context, %{context.phase => phase_context, old_phase: context.phase})
+  end
+end
+
