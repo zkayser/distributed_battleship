20779ba85be8127a8990719d5c567893d3c2a106
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-12-03 12:55:19 -0500

    fix bug with incorrect data in pattern

--- lib/battleship/phases/adding_ships.ex
+++ lib/battleship/phases/adding_ships.ex
@@ -1,7 +1,7 @@
 defmodule AddingShips do
   @moduledoc "allow players to tell the commander where there ships will be"
 
-  def tick(phase_context = %{trigger_pid: trigger_pid}) do
+  def tick(phase_context = %{service: %{trigger_pid: trigger_pid}}) do
     Phase.change?(trigger_pid, phase_context, :taking_turns)
   end
 end
--- test/battleship/phases/adding_ships_test.exs
+++ test/battleship/phases/adding_ships_test.exs
@@ -13,13 +13,13 @@ defmodule AddingShipsTest do
 
 
   test "initialize context", context do
-    phase_context = AddingShips.tick(%{trigger_pid: context.trigger_pid})
+    phase_context = AddingShips.tick(%{service: %{trigger_pid: context.trigger_pid}})
 
     refute Map.has_key?(phase_context, :new_phase)
   end
 
   test "second tick no timeout yet", context do
-    phase_context = %{trigger_pid: context.trigger_pid}
+    phase_context = %{service: %{trigger_pid: context.trigger_pid}}
       |> AddingShips.tick()
       |> AddingShips.tick()
 
@@ -29,7 +29,7 @@ defmodule AddingShipsTest do
   test "force a timeout and switch to new phase", context do
     Trigger.pull(context.trigger_pid)
 
-    phase_context = %{trigger_pid: context.trigger_pid}
+    phase_context = %{service: %{trigger_pid: context.trigger_pid}}
       |> AddingShips.tick()
 
     assert phase_context.new_phase == :taking_turns
