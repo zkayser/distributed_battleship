05e603c9563d704293845e24b472c0e49c0ab5e7
Edward Sumerfield <edward.sumerfield@plxis.com>
2017-12-03 12:42:01 -0500

    make adding ships switch to next phase on trigger command instead of a timer

--- lib/battleship/phases/adding_ships.ex
+++ lib/battleship/phases/adding_ships.ex
@@ -1,21 +1,8 @@
 defmodule AddingShips do
   @moduledoc "allow players to tell the commander where there ships will be"
 
-  def tick(phase_context = %{wait_max: wait_max, wait_start: wait_start}) do
-    wait_time = DateTime.diff(DateTime.utc_now, wait_start)
-    cond do
-      wait_time >= wait_max -> Phase.change(phase_context, :taking_turns)
-      true                  -> phase_context
-    end
-  end
-
-  def tick(phase_context) do
-    new_phase_context = %{
-      wait_max: 60,
-      wait_start: DateTime.utc_now
-    }
-
-    Map.merge(new_phase_context, phase_context)
+  def tick(phase_context = %{trigger_pid: trigger_pid}) do
+    Phase.change?(trigger_pid, phase_context, :taking_turns)
   end
 end
 
--- test/battleship/phases/adding_ships_test.exs
+++ test/battleship/phases/adding_ships_test.exs
@@ -1,24 +1,35 @@
 defmodule AddingShipsTest do
   use ExUnit.Case
 
-  test "initialize context" do
-    phase_context = AddingShips.tick(%{})
+  setup() do
+    trigger_pid = Trigger.start()
 
-    assert phase_context.wait_max == 60
-    assert phase_context.wait_start
+    on_exit(fn -> 
+      Trigger.stop()
+    end)
+
+    [trigger_pid: trigger_pid]
   end
 
-  test "second tick no timeout yet" do
-    phase_context = %{}
+
+  test "initialize context", context do
+    phase_context = AddingShips.tick(%{trigger_pid: context.trigger_pid})
+
+    refute Map.has_key?(phase_context, :new_phase)
+  end
+
+  test "second tick no timeout yet", context do
+    phase_context = %{trigger_pid: context.trigger_pid}
       |> AddingShips.tick()
       |> AddingShips.tick()
 
     refute Map.has_key?(phase_context, :new_phase)
   end
 
-  test "force a timeout and switch to new phase" do
-    phase_context = %{wait_max: 0}
-      |> AddingShips.tick()
+  test "force a timeout and switch to new phase", context do
+    Trigger.pull(context.trigger_pid)
+
+    phase_context = %{trigger_pid: context.trigger_pid}
       |> AddingShips.tick()
 
     assert phase_context.new_phase == :taking_turns
