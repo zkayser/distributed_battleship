5821068c4bb0d021cdf7f6d649e8104ac4ae9a6d
Edward Sumerfield <esumerfd@bitbashers.org>
2018-02-07 21:10:26 -0500

    start adding finish reporting.

--- lib/battleship/phases/finish.ex
+++ lib/battleship/phases/finish.ex
@@ -14,6 +14,18 @@ defmodule Finish do
     {:ok, registered_players} = Players.registered_players(players_pid)
     {:ok, ships} = Ocean.ships(ocean_pid)
 
+    report_stats(registered_players, ships)
+
+    notify_players(phase_context, registered_players, ships)
+  end
+
+  defp report_stats(registered_players, ships) do
+    IO.puts("NEED REPORTS HERE")
+    IO.inspect registered_players
+    IO.inspect ships
+  end
+
+  defp notify_players(phase_context, registered_players, ships) do
     winners_ships = Ships.floating(ships)
     winning_ship = List.first(winners_ships)
 
