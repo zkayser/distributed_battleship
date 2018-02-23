a45e0996b85911fa31701d243e1ece2934e70bc9
Edward Sumerfield <esumerfd@bitbashers.org>
2018-02-22 21:25:02 -0500

    add ip to clipboard to make it easy to run other comamnds

--- bin/start_battleships.sh
+++ bin/start_battleships.sh
@@ -7,5 +7,8 @@ if=`netstat -f inet -rn | grep default | awk '{ print $6 }'`
 ip=`ipconfig getifaddr $if`
 echo Listening on $if $ip
 
+# Save IP in clipboard
+echo -n $ip | pbcopy
+
 elixir --erl '-kernel inet_dist_listen_min 9000' --erl '-kernel inet_dist_listen_max 9100' --name commander@$ip -S mix run --no-halt
 
