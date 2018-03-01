a9cdd62c72a8979ef08e89801a046f2f1ed6d7cb
Edward Sumerfield <esumerfd@bitbashers.org>
2018-02-28 20:35:19 -0500

    reverse default public/private state

--- bin/ui.sh
+++ bin/ui.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ $# -lt 1 ]]; then
-  echo "usage: `basename $0` commander_ip [-public]"
+  echo "usage: `basename $0` commander_ip [--private]"
   exit 2
 fi
 
 commander_ip=$1
 shift
 
-visibility=private
-if [[ $1 == --public || $1 == -p ]]; then
+visibility=public
+if [[ $1 == --private || $1 == -p ]]; then
   shift
-  visibility=public
+  visibility=private
 fi
 
 elixir --erl '-kernel inet_dist_listen_min 9000' --erl '-kernel inet_dist_listen_max 9100' --name "ui$$@$commander_ip" -S mix battleship.ui $commander_ip $visibility $@ 
