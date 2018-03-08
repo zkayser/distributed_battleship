49fe4b56615982b9458b289dc7a23f62a3af209a
Edward Sumerfield <esumerfd@bitbashers.org>
2018-03-08 09:32:29 -0500

    add cookie

--- bin/start_iex.sh
+++ bin/start_iex.sh
@@ -10,4 +10,5 @@ fi
 commander_ip=$1
 playername=$2
 
-iex --erl '-kernel inet_dist_listen_min 9000' --erl '-kernel inet_dist_listen_max 9100' --name $playername@$commander_ip 
+iex --cookie battleships --erl '-kernel inet_dist_listen_min 9000' --erl '-kernel inet_dist_listen_max 9100' --name $playername@$commander_ip 
+
